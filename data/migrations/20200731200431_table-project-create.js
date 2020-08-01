adf71691941b0aa9bfb8b3ae32ee56dc733e8430
Anthony Ware <aware91coding@gmail.com>
2020-07-31 20:18:41 -0500

    created my schema and added db-config.js

--- data/migrations/20200731200431_table-project-create.js
+++ data/migrations/20200731200431_table-project-create.js
@@ -2,13 +2,54 @@ const { table } = require("../db-config");
 
 exports.up = function(knex) {
   return knex.schema
+  .createTable('task', tbl => {
+    tbl.increments();
+    tbl.string('description', 256)
+      .notNullable();
+    tbl.string('notes', 256)
+      .notNullable();
+  })
     .createTable('project', tbl => {
       tbl.increments();
-      tbl.string('name', 128);
-      tbl.string('description', 256);
+      tbl.string('name', 128)
+        .notNullable()
+        .unique();
+      tbl.string('description', 256)
+        .notNullable();
+      tbl.boolean('completed');
+      tbl.integer('task_id')
+        .unsigned()
+        .notNullable()
+        .references('task.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+    .createTable('resource', tbl => {
+      tbl.increments();
+      tbl.string('name', 128)
+        .notNullable()
+        .unique();
+    })
+    .createTable('project_resource', tbl => {
+      tbl.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+        tbl.integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resource.id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
     })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('project_resource')
+    .dropTableIfExists('resource')
+    .dropTableIfExists('project')
+    .dropTableIfExists('task')
 };
