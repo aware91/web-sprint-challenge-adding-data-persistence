67da789956e697a6f2055931c113c4571c43c16a
Anthony Ware <aware91coding@gmail.com>
2020-07-31 21:09:36 -0500

    have all seeds working.

--- data/seeds/05-project_task.js
+++ data/seeds/05-project_task.js
@@ -1,18 +1,18 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('project_resource').del()
+  return knex('project_task').del()
     .then(function () {
       // Inserts seed entries
-      return knex('project_resource').insert([
-        {project_id: '1', resource_id: '1'},
-        {project_id: '1', resource_id: '2'},
-        {project_id: '1', resource_id: '3'},
-        {project_id: '1', resource_id: '4'},
-        {project_id: '1', resource_id: '5'},
-        {project_id: '2', resource_id: '1'},
-        {project_id: '2', resource_id: '2'},
-        {project_id: '2', resource_id: '6'},
+      return knex('project_task').insert([
+        {project_id: '1', task_id: '1'},
+        {project_id: '1', task_id: '2'},
+        {project_id: '1', task_id: '3'},
+        {project_id: '1', task_id: '4'},
+        {project_id: '1', task_id: '5'},
+        {project_id: '2', task_id: '1'},
+        {project_id: '2', task_id: '2'},
+        {project_id: '2', task_id: '6'},
       ]);
     });
 };
